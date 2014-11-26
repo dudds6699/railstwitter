1daecd6465751f9a2d69958ab71ef21535b5ce10
dudds6699 <dudds6699@hotmail.com>
2014-11-26 23:46:35 +0000

    so it begins i can now collect tweets with the rails model and nothing can stop me mu hahahahahahahahahaha

--- TweetFiller/index.js
+++ TweetFiller/index.js
@@ -6,6 +6,9 @@ var pg = require('pg');
 var sql = require("squel").useFlavour('postgres');
 var Twit = require('twit');
 
+var T = new Twit(connections.Tweet);
+var conString = connections.db;
+
 function makeSQL(tweet){
     
     //just some simple parsing for the special geometry functions i use to store the point
@@ -16,10 +19,41 @@ function makeSQL(tweet){
     var query =
         {
                // text : "INSERT INTO tweets (username, tweet, geo) Values ($1,$2,st_setsrid(st_makepoint($3,$4),"+srid+"))",
-				text : "INSERT INTO tweets (twitterID, tweetID, username, place, coords, created_at, tweettext) Values ($1,$2, $3, $4 ,st_setsrid(st_makepoint($5,$6),"+srid+"), $7)",
-				values:[tweet.user.id_str ,tweet.id_str, tweet.user.screen_name, tweet.place.name, coordinates[1] , coordinates[0], date, tweet.text  ]
+				text : 'INSERT INTO tweets ("twitterID", "tweetID", username, place, coords, created_at, tweettext) Values ($1,$2, $3, $4 ,st_setsrid(st_makepoint($5,$6),'+srid+'), $7, $8)',
+				values:[tweet.user.id_str ,tweet.id_str, tweet.user.screen_name, tweet.place.name, coordinates[1] , coordinates[0], new Date(), tweet.text  ]
         };
         
     return query;
 
 }
+
+function connectToDb(sql){
+
+        
+        console.log(sql.values)
+        
+        var query = client.query(sql);
+        
+        query.on('end', function (result){
+            console.log(result);
+        });
+                
+}
+
+//my stream
+var stream = T.stream('statuses/sample');
+
+//streams the tweet and then goes and gets it
+stream.on('tweet', function(tweet) {
+    if (tweet.geo !== null)
+        connectToDb(makeSQL(tweet));
+});
+
+var client = new pg.Client(conString);
+client.connect();
+//client.on('drain', client.end.bind(client));
+
+client.on('error', function(error){
+    console.log(error);
+    client.end.bind(client);
+});
